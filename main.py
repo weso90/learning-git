8eeccf74d322afc5fa1858575a616de651a3e065
Maksym Wesolowski <weso.pl@gmail.com>
2025-03-20 20:51:46 +0100

    Add main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#ZADANIE 1
+
+shopping_list = {
+    "piekarnia": ["chleb", "bułki", "pączek"],
+    "warzywniak": ["marchew", "seler", "rukola"]
+}
+
+for i, j in shopping_list.items():
+    print(f"Idę do {i.capitalize()} i kupuję tam: {[j.capitalize() for j in j]}")
+
+print(f"W sumie kupuję {[sum(len(i) for i in shopping_list.values())]} towarów")
