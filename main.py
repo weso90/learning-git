f820716aa82704252182a8a7207c55b3df5f94c4
Maksym Wesolowski <weso.pl@gmail.com>
2025-03-23 14:29:01 +0100

    added print

--- main.py
+++ main.py
@@ -10,3 +10,5 @@ for i, j in shopping_list.items():
     print(f"Idę do {i.capitalize()} i kupuję tam: {[j.capitalize() for j in j]}")
 
 print(f"W sumie kupuję {[sum(len(i) for i in shopping_list.values())]} towarów")
+
+print("trzeci branch, sprawdzam czy merge będzie działał")
